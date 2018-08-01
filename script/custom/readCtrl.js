1162c16a51d4188fff05144f9d5ace620445fd70
pawankotak <kotakpawan@gmail.com>
2018-08-01 18:37:03 +0530

    Effort for Clean Code in readCtrl.js file

--- script/custom/readCtrl.js
+++ script/custom/readCtrl.js
@@ -1,56 +1,59 @@
 var varReadController = function ($scope, $http) {
 
     // console.log("hello!");
-    $scope.errorMessage = "";  
+    $scope.errorMessage = "";
 
     $scope.test = function () {
         //  getData($scope.searchTerm);
-        getField($scope.searchTerm);
+        getListField($scope.searchTerm);
     };
     $scope.searchItem = function () {
-        getData($scope.searchTerm);
+        getListData($scope.searchTerm);
     };
-    $scope.boolShowFieldArea=false;
+    $scope.boolShowFieldArea = false;
     $scope.Fields = [];
 
-    // Function to Get List Data
-    function getData(listName) {
-         fnReset();
-        var selectedField = selectFieldInList();
-        var fixed = '?$select=';
-        var siteURL = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getByTitle('" + listName + "')/items";
 
-        var completeURL = siteURL + fixed + selectedField
+    function getListData(listName)
+    {
+        fnResetValue();
+        var selectedField = getSelectedFieldInList();
+        var restAPI_Select = '?$select=';
+        var siteRest_API_URL = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getByTitle('" + listName + "')/items";
+
+        var completeURL = siteRest_API_URL + restAPI_Select + selectedField
         $http.get(completeURL).
-            then(function (response) {
+            then(function (response)
+            {
                 console.log(response.data);
-                $scope.listData = response.data.value;               
+                $scope.listData = response.data.value;
             }, function (error) {
                 $scope.errorMessage = "Error Detail: " + error.data['odata.error'].message.value;
                 console.log("Error handling");
             });
     }
 
-    // Function to Get List Field
-    function getField(listName) {
-         fnReset();
-        var siteURL = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/GetByTitle('" + listName + "')/Fields";
-        $http.get(siteURL).
-            then(function (response) {               
-                // console.log("Field Detail"+response.data); 
+    function getListField(listName)
+     {
+        fnResetValue();
+        var siteRest_API_URL = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/GetByTitle('" + listName + "')/Fields";
+        $http.get(siteRest_API_URL).
+            then(function (response)
+             {
+
                 $scope.listField = response.data.value;
                 response.data.value.forEach(function (element) {
                     console.log(element.Title);
                 }, this);
-            $scope.boolShowFieldArea = true;
+                $scope.boolShowFieldArea = true;
             }, function (error) {
                 $scope.errorMessage = "Error Detail: " + error.data['odata.error'].message.value;
                 console.log("Error handling");
             });
     }
 
-    //Function Get Selection Clause for Rest API for Item Selected
-    function selectFieldInList() {
+    function getSelectedFieldInList()
+     {
         var fieldsFromList = '';
         $scope.Fields = [];
         $scope.listField.forEach(function (element) {
@@ -66,10 +69,11 @@ var varReadController = function ($scope, $http) {
         fieldsFromList = fieldsFromList.substring(0, fieldsFromList.length - 1);
         return fieldsFromList;
     }
-    //Function Reset Error Message
-    function fnReset(){
-        $scope.errorMessage='';
-    }  
+
+    function fnResetValue()
+     {
+        $scope.errorMessage = '';
+    }
 
 }
 
